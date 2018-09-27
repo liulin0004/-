beab82821a601c449526035f8f2648a19377f331
wangbazhushou <40532495+liulin0004@users.noreply.github.com>
2018-09-27 13:29:58 +0800

    go语言
    go语言的第一次使用

---
+++ hello.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func variale(){
+	var a int
+	var b string
+	a += 10
+	b = "111"
+	fmt.Println(a,b)
+}
+func main(){
+	var c,d int = 20,22
+	f := 20
+	fmt.Println("hello word",c,d,f)
+	variale()
+}
